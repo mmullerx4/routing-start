4939b17b63ff3710f9dc83f7f97dfcfa8d980858
mmullerx4 <mmullerx4@gmail.com>
2024-10-22 19:53:06 -0400

    w06-passingQueryParams & Fragments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
-    this.router.navigate(['/servers']);
+  //navigate to servers, pass id, edit
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
 }
