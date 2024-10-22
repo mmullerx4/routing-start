b235b2c0ac9bb0b2f6a76fa5b036c210180470dc
mmullerx4 <mmullerx4@gmail.com>
2024-10-22 12:41:31 -0400

    w06-fetching route params

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,19 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  //ActivatedRoute injects the current active route
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) {
+
+    }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    //this.router.navigate(['servers'], {relativeTo: this.route}); //relativeTo to tell navigate figure where we are compared to the root or active route
   }
 
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  //inject & get access to the currently loaded route
+  constructor(private routea: ActivatedRoute) { }
 
+  //snapshot of currently active route we have params of object
   ngOnInit() {
+    this.user = {
+      id: this.routea.snapshot.params['id'],
+      name: this.routea.snapshot.params['name']
+    }
   }
 
 }
