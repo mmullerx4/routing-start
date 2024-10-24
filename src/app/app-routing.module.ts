507fa34dd11cc82c5754579d77a9eade1b502ce0
mmullerx4 <mmullerx4@gmail.com>
2024-10-24 13:10:05 -0400

    w06-routing tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { HomeComponent } from "./home/home.component";
+import { AuthGuard } from "./auth-guard.service";
 
 
 
@@ -17,7 +18,10 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent }
   ] },
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers',
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent },
   ] },
